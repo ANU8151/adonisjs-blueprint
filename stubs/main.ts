8f0f17d67ec5afae657f5b77154344b9e0ce3b0e
ANU8151 <gia.nizharadze@gmail.com>
2026-03-28 17:30:12 +0400

    fix: ensure stubs are correctly resolved by making stubs/main.ts an entry point

--- stubs/main.ts
+++ stubs/main.ts
@@ -1,8 +1,7 @@
-import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 /**
  * Path to the root directory where the stubs are stored. We use
- * this path within commands and the configure hook
+ * this path within commands and the configure hook.
  */
-export const stubsRoot = join(fileURLToPath(new URL('../', import.meta.url)), 'stubs')
+export const stubsRoot = fileURLToPath(new URL('./', import.meta.url))
