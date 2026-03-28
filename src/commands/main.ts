9d4f6a357a57c40fe057dc885a85ba76b788a454
ANU8151 <gia.nizharadze@gmail.com>
2026-03-28 18:24:12 +0400

    fix: align command registration with official adonisjs packages

--- src/commands/main.ts
+++ src/commands/main.ts
@@ -1,10 +1,13 @@
-const commands = [
-  () => import('./build.js'),
-  () => import('./erase.js'),
-  () => import('./trace.js'),
-  () => import('./stubs.js'),
-  () => import('./init.js'),
-]
+import BuildBlueprint from './build.js'
+import EraseBlueprint from './erase.js'
+import TraceBlueprint from './trace.js'
+import EjectStubs from './stubs.js'
+import InitBlueprint from './init.js'
 
-export { commands }
-export default commands
+export const commands = [
+  BuildBlueprint,
+  EraseBlueprint,
+  TraceBlueprint,
+  EjectStubs,
+  InitBlueprint,
+]
